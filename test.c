0278405b0c69386332aaf2bcfeac7edd677679dc
sini1995 <sreeshasuresh2@gmail.com>
2018-03-19 15:31:23 +0530

    msg for the commit

---
+++ test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+char a[20],b[20];
+int i;
+printf("enter  a text");
+for(i=0;i<20;i++)
+{
+scanf("%c",&a[i]);
+}
+printf("enter another text");
+for(i=0;i<20;i++)
+{
+scanf("%c",&b[i]);
+}
+for(i=0;i<20;i++)
+{
+if(a[i]!=b[i])
+{
+printf("not equal");
+break;
+}
+}
+}
